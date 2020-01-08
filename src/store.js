0ef91c37e4abc93921d89d30bbadf1abbde18727
chenlei <lei.chen1@thoughtworks.com>
2020-01-08 22:30:49 +0800

    support redux-dev-tools

--- src/store.js
+++ src/store.js
@@ -1,9 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { promiseMiddleware } from 'redux-actions-helper';
 
 import defaultReducers from './reducers';
 
-export const store = createStore(defaultReducers, applyMiddleware(ReduxThunk, promiseMiddleware));
+const middleware = [
+    ReduxThunk,
+    promiseMiddleware,
+];
+
+let composeEnhancers = compose;
+
+if (process.env.REACT_APP_ENV === 'debug') {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+}
+
+export const store = createStore(defaultReducers, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;
